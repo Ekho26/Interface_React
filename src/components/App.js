f69a30e402aea8bc481b249730c5d8e3adcc1a80
Ekho26 <munteanu.ionut1991@gmail.com>
2021-03-31 16:53:57 +0300

    Adding the search bar

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,44 @@ import AddAppointments from "./AddAppointments";
 import SearchAppointments from "./SearchAppointments";
 import ListAppointments from "./ListAppointments";
 
+import {without} from 'lodash';
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      myAppointments: []
+      myAppointments: [],
+      formDisplay: false,
+      lastIndex: 0
     };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+  }
+
+  toggleForm() {
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    })
+  }
+
+  addAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex + 1
+    })
+  }
+
+  deleteAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    });
   }
 
   componentDidMount(){
@@ -18,6 +50,8 @@ class App extends Component {
       .then(response => response.json())
       .then(result => {
         const apts = result.map(item =>{
+          item.aptId = this.state.lastIndex;
+          this.setState({lastIndex: this.state.lastIndex + 1})
           return item;
         })
         this.setState({
@@ -33,9 +67,15 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments />
+                <AddAppointments 
+                  formDisplay={this.state.formDisplay}
+                  toggleForm={this.toggleForm}
+                  addAppointment={this.addAppointment}
+                />
                 <SearchAppointments />
-                <ListAppointments appointments={this.state.myAppointments}/>
+                <ListAppointments appointments={this.state.myAppointments}
+                                  deleteAppointment={this.deleteAppointment}
+                />
               </div>
             </div>
           </div>
--- src/components/SearchAppointments.js
+++ src/components/SearchAppointments.js
@@ -2,7 +2,36 @@ import React, { Component } from 'react'
 
 export class SearchAppointments extends Component {
     render() {
-        return <div>Search Appointments</div>;
+        return (
+
+            <div className="search-appointments row justify-content-center my-4">
+                <div className="col-md-6">
+                    <div className="input-group">
+                        <input 
+                            id="SearchApts"
+                            type="text"
+                            className="form-control"
+                            aria-label="Search Appointments" 
+                        />
+                        <div className="input-group-append">
+                            <button
+                                type="button"
+                                className="btn btn-primary dropdown-toggle"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                Sort by: <span className="caret" />
+                            </button>
+                            <div className="sort-menu dropdown-menu dropdown-menu-right">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
     }
 }
 
